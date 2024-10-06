edd6701a32eb7da5856d9bdba697e0303020fef8
Serhii Kurlykin <kurlykin83@gmail.com>
2024-10-06 16:44:43 +0200

    test

--- Classwork/SummarySession2/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Cat barsik = new Cat("КОТ", 20);
-        System.out.println(barsik);
-        barsik.run();
-        Cat murka = new Cat("МУРКА", 20);
-        murka.run();
-    }}
