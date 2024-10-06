a86fdfd8980b9d2a06b089081dc00604d923764d
Serhii Kurlykin <kurlykin83@gmail.com>
2024-10-06 17:39:06 +0200

    test

--- src/classWork/lesson5/Testfifth.java
+++ src/classWork/lesson5/Testfifth.java
@@ -1,16 +1,16 @@
-//package classWork.lesson5;
-//
-//public class Testfifth {
-//    public static <Catthird> void main(String[] args) {
-//
-//        Catthird myCat = new Catthird( "myCat", 5, "brown");
-//        System.out.println(myCat.name + " " + myCat.age + " " + myCat.color);
-//
-//        Catthird ben = new Catthird( "ben", 12, "white");
-//        System.out.println(ben.name + " " + ben.age + " " + ben.color);
-//
-//
-//
-//    }
-//}
+package classWork.lesson5;
+
+public class Testfifth {
+    public static void  main(String[] args) {
+
+        Catfifth myCat = new Catfifth( "myCat", 5, "brown");
+        System.out.println(myCat.name + " " + myCat.age + " " + myCat.color);
+
+        Catfifth ben = new Catfifth( "ben", 12, "white");
+        System.out.println(ben.name + " " + ben.age + " " + ben.color);
+
+
+
+    }
+}
 
